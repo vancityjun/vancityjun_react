7fe080d18ab14b0c0e30e7086c3a702788b39e5b
vancityjun <vancityjun@gmail.com>
2026-04-27 23:23:10 -0700

    chore: organize project media assets

--- src/helper/projectImages.js
+++ src/helper/projectImages.js
@@ -1,14 +1,14 @@
 // Vite's import.meta.glob replaces dynamic require() for images.
 // `eager: true` returns resolved modules; `import: 'default'` returns URL strings.
-const modules = import.meta.glob('../img/*.{png,jpg,jpeg,svg}', {
+const modules = import.meta.glob('../img/**/*.{png,jpg,jpeg,svg}', {
   eager: true,
   import: 'default',
 })
 
 const projectImages = {}
 for (const path in modules) {
-  const filename = path.split('/').pop()
-  projectImages[filename] = modules[path]
+  const relativePath = path.replace('../img/', '')
+  projectImages[relativePath] = modules[path]
 }
 
 export default projectImages
